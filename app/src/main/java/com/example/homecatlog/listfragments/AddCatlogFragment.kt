72f20bec21114b02ae9ee3097abfa401ef2132c3
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-08-24 17:26:38 +0530

    set onclick for backButton in AddCatlogFragment

--- app/src/main/java/com/example/homecatlog/listfragments/AddCatlogFragment.kt
+++ app/src/main/java/com/example/homecatlog/listfragments/AddCatlogFragment.kt
@@ -1,23 +1,33 @@
 package com.example.homecatlog.listfragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.homecatlog.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.homecatlog.databinding.FragmentAddCatlogBinding
 
 class AddCatlogFragment : Fragment() {
 
+    private lateinit var binding: FragmentAddCatlogBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_catlog, container, false)
+        binding = FragmentAddCatlogBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            backButton.setOnClickListener { navigateToBackFragment() }
+        }
+    }
+
+    private fun navigateToBackFragment() {
+        findNavController().popBackStack()
     }
 }
