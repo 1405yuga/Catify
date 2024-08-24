5d43b2738eb196069675df09ba57cfbc44803d67
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-08-24 17:15:03 +0530

    established navigation from CategoryListFragment to AddCategoryFragment

--- app/src/main/java/com/example/homecatlog/listfragments/CategoryListFragment.kt
+++ app/src/main/java/com/example/homecatlog/listfragments/CategoryListFragment.kt
@@ -1,23 +1,35 @@
 package com.example.homecatlog.listfragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.homecatlog.R
+import com.example.homecatlog.databinding.FragmentCategoryListBinding
 
 class CategoryListFragment : Fragment() {
 
+    private lateinit var binding: FragmentCategoryListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_list, container, false)
+        binding = FragmentCategoryListBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            addCatlog.setOnClickListener { navigateToFragment(R.id.addCatlogFragment) }
+        }
+    }
+
+    private fun navigateToFragment(fragmentId: Int) {
+        findNavController().apply {
+            navigate(fragmentId)
+        }
     }
 }
