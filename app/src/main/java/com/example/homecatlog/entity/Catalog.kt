cd52eb453ecb9d3070d6e7075a84e8de63217a8f
1405yuga <yugav2001@gmail.com>
2024-09-18 17:19:46 +0530

    removed room database classes

--- app/src/main/java/com/example/homecatlog/entity/Catalog.kt
+++ app/src/main/java/com/example/homecatlog/entity/Catalog.kt
@@ -1,10 +1,7 @@
 package com.example.homecatlog.entity
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "Catalog")
 data class Catalog(
-    @PrimaryKey val category: String,
+    val category: String,
     val homeItems: List<HomeItem>
 )
