c10f097e3ee2edc8ab0f8c2be4c424be177bf7f9
1405yuga <yugav2001@gmail.com>
2024-09-20 15:45:39 +0530

    implemented Delete catalog on swipe

--- app/src/main/java/com/example/homecatlog/ui/listfragments/CatalogListFragment.kt
+++ app/src/main/java/com/example/homecatlog/ui/listfragments/CatalogListFragment.kt
@@ -5,9 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.homecatlog.R
 import com.example.homecatlog.databinding.FragmentCatalogListBinding
 import com.example.homecatlog.network.BaseApplication
@@ -17,6 +20,7 @@ class CatalogListFragment : Fragment() {
 
     private lateinit var binding: FragmentCatalogListBinding
     private val TAG = this.javaClass.simpleName
+    private lateinit var swipeHelper: ItemTouchHelper
     private val viewModel: CatalogViewModel by activityViewModels {
         CatalogViewModel.CatalogViewModelFactory((activity?.application as BaseApplication).database.getCatalogDao())
     }
@@ -34,6 +38,35 @@ class CatalogListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         applyBinding()
+        applyObservers()
+        applyDeleteOnSwipe()
+    }
+
+    private fun applyDeleteOnSwipe() {
+        swipeHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val catalog = catalogListAdapter.currentList[viewHolder.adapterPosition]
+                viewModel.removeCatalog(catalog,
+                    onSuccess = { Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show() },
+                    onFailure = { Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show() })
+                catalogListAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+
+        })
+        swipeHelper.attachToRecyclerView(binding.categoryRecyclerView)
+    }
+
+    private fun applyObservers() {
         viewModel.allCatalogs.observe(viewLifecycleOwner) {
             Log.d(TAG, "Catalogs list :${it.size}")
             if (it.isEmpty()) {
