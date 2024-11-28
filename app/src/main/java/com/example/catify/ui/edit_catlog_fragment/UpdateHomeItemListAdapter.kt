a118b52d8a8ded250739af7d6c7c3c411c00ef06
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-11-28 13:13:18 +0530

    added individual binding

--- app/src/main/java/com/example/catify/ui/edit_catlog_fragment/UpdateHomeItemListAdapter.kt
+++ app/src/main/java/com/example/catify/ui/edit_catlog_fragment/UpdateHomeItemListAdapter.kt
@@ -36,84 +36,80 @@ class UpdateHomeItemListAdapter(
         }
     }
 
-    class HomeItemViewHolder(private val binding: CardUpdateHomeItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class HomeItemViewHolder(
+        private val binding: CardUpdateHomeItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        val TAG = this.javaClass.simpleName
+
         fun bind(
             catalogListItem: CatalogListItem,
             addItemView: (Int, String) -> Unit,
             removeItemView: (Int, String) -> Unit
         ) {
-            val TAG = this.javaClass.simpleName
             Log.d(TAG, "ViewHolder called -----")
-
-            binding.apply {
-                itemName.apply {
-                    setText(catalogListItem.itemName)
-
-                    itemName.setOnKeyListener { view, i, keyEvent ->
-                        //on enter press - add new textview with remaining text
-                        if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
-                            val nextPos = adapterPosition + 1
-                            val cursorPos = itemName.selectionStart
-                            val mainText = catalogListItem.itemName.slice(0 until cursorPos)
-                            val remainingText =
-                                catalogListItem.itemName.slice(cursorPos until catalogListItem.itemName.length)
-                            Log.d(TAG, remainingText)
-                            addItemView(nextPos, remainingText)
-                            setText(mainText)
-                            true
-                        } else if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_DEL) {
-                            if (itemName.selectionStart == 0 && adapterPosition > 0) {
-                                val remainingText = itemName.text.toString()
-                                val prevPos = adapterPosition - 1
-                                removeItemView(prevPos, remainingText)
-                            }
-                            true
-                        } else false
+            binding.itemName.setText(catalogListItem.itemName)
+            binding.itemName.setOnKeyListener { view, i, keyEvent ->
+                //on enter press - add new textview with remaining text
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                    val nextPos = adapterPosition + 1
+                    val cursorPos = binding.itemName.selectionStart
+                    val mainText = catalogListItem.itemName.slice(0 until cursorPos)
+                    val remainingText =
+                        catalogListItem.itemName.slice(cursorPos until catalogListItem.itemName.length)
+                    Log.d(TAG, remainingText)
+                    addItemView(nextPos, remainingText)
+                    binding.itemName.setText(mainText)
+                    true
+                } else if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_DEL) {
+                    if (binding.itemName.selectionStart == 0 && adapterPosition > 0) {
+                        val remainingText = binding.itemName.text.toString()
+                        val prevPos = adapterPosition - 1
+                        removeItemView(prevPos, remainingText)
                     }
+                    true
+                } else false
+            }
 
-                    //set focus to last character
-                    itemName.setOnFocusChangeListener { view, hasFocus ->
-                        if (hasFocus) {
-                            val editText = view as EditText
-                            editText.setSelection(editText.length())
-                        }
-                    }
-
-                    //set entered value
-                    itemName.addTextChangedListener(object : TextWatcher {
-                        override fun beforeTextChanged(
-                            p0: CharSequence?,
-                            p1: Int,
-                            p2: Int,
-                            p3: Int
-                        ) {
-                        }
-
-                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                            if (p0!!.trim().isNotEmpty()) catalogListItem.itemName = p0.toString()
-                            Log.d(TAG, "onTextChanged $p0")
-                        }
-
-                        override fun afterTextChanged(p0: Editable?) {}
-
-                    })
+            //set focus to last character
+            binding.itemName.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus) {
+                    val editText = view as EditText
+                    editText.setSelection(editText.length())
                 }
-                fun refreshCount() {
-                    quantity.text = catalogListItem.availableStock.toString()
+            }
+
+            //set entered value
+            binding.itemName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    p0: CharSequence?,
+                    p1: Int,
+                    p2: Int,
+                    p3: Int
+                ) {
                 }
-                addQuantity.setOnClickListener {
-                    catalogListItem.availableStock++
-                    refreshCount()
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (p0!!.trim().isNotEmpty()) catalogListItem.itemName = p0.toString()
+                    Log.d(TAG, "onTextChanged $p0")
                 }
-                subQuantity.setOnClickListener {
-                    catalogListItem.decreaseQty()
-                    refreshCount()
-                }
+
+                override fun afterTextChanged(p0: Editable?) {}
+
+            })
+
+            fun refreshCount() {
+                binding.quantity.text = catalogListItem.availableStock.toString()
+            }
+            binding.addQuantity.setOnClickListener {
+                catalogListItem.availableStock++
                 refreshCount()
             }
+            binding.subQuantity.setOnClickListener {
+                catalogListItem.decreaseQty()
+                refreshCount()
+            }
+            refreshCount()
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeItemViewHolder {
