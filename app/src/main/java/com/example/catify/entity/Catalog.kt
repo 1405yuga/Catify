48a7be52cb989da7de8800eca8f8e4b531c0a393
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-11-28 12:29:49 +0530

    created deleteWithTransferAndReturnCursorIndex

--- app/src/main/java/com/example/catify/entity/Catalog.kt
+++ app/src/main/java/com/example/catify/entity/Catalog.kt
@@ -8,5 +8,25 @@ data class Catalog(
     @PrimaryKey(autoGenerate = true)
     val catalogId: Int = 0,
     var category: String,
-    var catalogListItems: List<CatalogListItem>
-)
+    var catalogListItems: MutableList<CatalogListItem>
+) {
+    // TODO: test 
+    fun deleteWithTransferAndReturnCursorIndex(index: Int): Int? {
+        try {
+            val cursorIndex: Int?
+            if (index > 0) {
+                val toDelete = this.catalogListItems.getOrNull(index = index)
+                val destination = this.catalogListItems.getOrNull(index = index - 1)
+                cursorIndex = destination?.itemName?.length
+                destination?.itemName += toDelete?.itemName ?: ""
+            } else {
+                cursorIndex = null
+            }
+            this.catalogListItems.removeAt(index = index)
+            return cursorIndex
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+}
