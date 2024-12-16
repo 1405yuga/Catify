5b744411ad77124eb8ed0f78157fffdc9f3a3a8e
1405yuga <yugav2001@gmail.com>
2024-12-16 13:23:14 +0530

    created basic ui-outline for ShoppingCartFragment

--- app/src/main/java/com/example/catify/ui/shopping_cart_fragment/ShoppingCartFragment.kt
+++ app/src/main/java/com/example/catify/ui/shopping_cart_fragment/ShoppingCartFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.catify.R
 import com.example.catify.databinding.FragmentShoppingCartBinding
 
@@ -21,5 +22,10 @@ class ShoppingCartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        applyBindings()
+    }
+
+    private fun applyBindings() {
+        binding.backButton.setOnClickListener { findNavController().popBackStack() }
     }
 }
