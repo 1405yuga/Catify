76b753b6e4085ed2c64ce40cb98a7a4e3eb468c8
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-12-17 11:56:34 +0530

    entities for shoppingcart

--- app/src/main/java/com/example/catify/entity/cart/CartItem.kt
+++ app/src/main/java/com/example/catify/entity/cart/CartItem.kt
@@ -1,11 +1,6 @@
 package com.example.catify.entity.cart
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class CartItem(
-    @PrimaryKey(autoGenerate = true)
     val itemId: Int,
     var itemName: String,
     var stock: Int,
---
+++ app/src/main/java/com/example/catify/entity/cart/ShoppingCart.kt
@@ -0,0 +1,5 @@
+package com.example.catify.entity.cart
+
+data class ShoppingCart(
+    val cartItemList: MutableList<CartItem>
+)
